74111a735202650579641e1ddb0f6d70d22a80f5
Liam Murray <liam@murray.to>
2024-01-22 03:42:58 -0600

    Fix: register disable on /register

--- apps/web/src/app/register/page.tsx
+++ apps/web/src/app/register/page.tsx
@@ -9,6 +9,7 @@ import Navbar from "@/components/shared/Navbar";
 import Link from "next/link";
 import { kv } from "@vercel/kv";
 import { parseRedisBoolean } from "@/lib/utils/server/redis";
+import { Button } from "@/components/shadcn/ui/button";
 
 export default async function Page() {
 	const { userId } = auth();
@@ -57,6 +58,33 @@ export default async function Page() {
 			</>
 		);
 	}
+
+	return (
+		<main className="flex flex-col min-h-screen items-center justify-center px-2">
+			<div className="fixed left-1/2 top-[calc(50%+7rem)] overflow-x-hidden h-[40vh] w-[800px] max-w-screen -translate-x-1/2 -translate-y-1/2 scale-150 bg-hackathon opacity-30 blur-[100px] will-change-transform" />
+			<h2 className="text-4xl font-extrabold">{c.hackathonName}</h2>
+			<h1 className="text-6xl md:text-8xl pb-5 mb-10 font-extrabold text-hackathon dark:text-transparent dark:bg-gradient-to-t dark:from-hackathon/80 dark:to-white dark:bg-clip-text">
+				Registration
+			</h1>
+			<div className="w-full max-w-[500px] flex gap-y-4 flex-col justify-center items-center px-5 dark:bg-white/[0.08] bg-white backdrop-blur transition rounded-xl aspect-video relative z-10 ">
+				<h2 className="font-black text-2xl">Registration Is Currently Closed</h2>
+				<p className="text-center font-bold">
+					If you believe this is a mistake or have any questions, feel free to reach out to us at{" "}
+					{c.issueEmail}!
+				</p>
+
+				<Link href={"/"}>
+					<Button>Return Home</Button>
+				</Link>
+				<p className="text-sm absolute bottom-0 text-center pb-2">
+					Already registered?{" "}
+					<Link className="underline" href={"/sign-in"}>
+						Sign-in.
+					</Link>
+				</p>
+			</div>
+		</main>
+	);
 }
 
 export const runtime = "edge";
