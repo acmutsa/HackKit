9f973a88ecc47a2e607cb3bba90c863e666e009d
Christian Walker <christiandwalker2021@gmail.com>
2024-01-25 22:00:15 -0600

    Added filler section

--- apps/web/src/components/landing/Filler.tsx
+++ apps/web/src/components/landing/Filler.tsx
@@ -1,8 +1,6 @@
 export default function Filler(){
     return (
-        // TODO The size of the div needs to be made responsive
-        <div className="w-full min-h-[18.5rem] sm:h-[20.15rem] md:min-h-screen lg:min-h-screen bg-[url('/img/landing/filler.svg')] bg-no-repeat bg-cover">
-            
-        </div>
-    )
+    // Christian:  Purpose is to add a bit of fluff and affect between the hero and about section
+      <div className="w-full min-h-[50vh] md:min-h-[70vh] lg:min-h-screen bg-[url('/img/landing/filler.svg')] bg-no-repeat bg-cover"></div>
+    );
 }
