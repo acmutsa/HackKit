56b46dd0b46a9c0af708b11b49f318dfd76260df
joshuasilva414 <joshuasilva414@gmail.com>
2024-01-27 12:19:46 -0600

    Partners section in progress

--- apps/web/src/components/landing/Partners.tsx
+++ apps/web/src/components/landing/Partners.tsx
@@ -1,17 +1,30 @@
 import { FaArrowAltCircleRight } from "react-icons/fa";
 import Link from "next/link";
+import partnerData from "./partners.json";
+import PartnerCard from "./PartnerCard";
 
-export default function Partners() {
-	return (
-		<section className="bg-[#3D5A30] min-h-screen w-full flex flex-col gap-y-36 items-center justify-center">
-			<h1 className="text-6xl font-bold font-oswald italic text-[#7D9037] text-center">
-				MORE HERE SOON!
-			</h1>
-			{/* <Link href="/register">
+type Partner = {
+  name: string;
+  logo: string;
+  url: string;
+};
+
+export default async function Partners() {
+  return (
+    <section className="bg-[#3D5A30] min-h-screen w-full flex flex-col gap-y-36 items-center justify-center">
+      <h1 className="text-6xl font-bold font-oswald italic text-[#7D9037] text-center">
+        Partners
+      </h1>
+
+      {partnerData.partners.map((partner: Partner) => (
+        <PartnerCard key={partner.name} {...partner} />
+      ))}
+
+      {/* <Link href="/register">
 				<button className="bg-[#FEF2E6] text-lg text-[#3D5A30] font-mono font-bold px-5 py-2 rounded flex items-center gap-x-2">
 					Register Now <FaArrowAltCircleRight />
 				</button>
 			</Link> */}
-		</section>
-	);
+    </section>
+  );
 }
