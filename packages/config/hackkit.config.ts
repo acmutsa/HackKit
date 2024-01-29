768c2755a6aca39ee956bf5e138f4fe1972e6f1b
Liam Murray <liam@murray.to>
2024-01-29 17:57:07 -0600

    Fix: hackkit config

--- packages/config/hackkit.config.ts
+++ packages/config/hackkit.config.ts
@@ -502,7 +502,6 @@ export const schools = [
 	"Thomas Edison State College",
 	"Trent University",
 	"Trinity College",
-	"Trinity University - San Antonio",
 	"Trinity Valley School",
 	"Troy University",
 	"Tufts University",
