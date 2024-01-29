d687374805e4c395054b83c0ae1fc9602f7c6b28
Liam Murray <liam@murray.to>
2024-01-29 17:45:22 -0600

    Temp: comment out team

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -24,9 +24,9 @@ export default function Home() {
 				<Hero />
 				<Filler />
 				<About />
-				{/* Commenting Out Until All Partners Added */}
+				{/* Commenting Out Until All Partners / Teams completed */}
 				{/* <Partners /> */}
-				<Team />
+				{/* <Team /> */}
 				<Footer />
 			</main>
 		</>
