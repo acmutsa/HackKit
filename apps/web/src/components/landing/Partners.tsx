8b18c2df039760a5a3d6080f41e77c923cfc1fdc
Christian Walker <christiandwalker2021@gmail.com>
2024-01-30 01:13:35 -0600

    Added extra assets

--- apps/web/src/components/landing/Partners.tsx
+++ apps/web/src/components/landing/Partners.tsx
@@ -19,11 +19,12 @@ type Partner = {
 
 export default async function Partners() {
   return (
-    <section className="bg-[#3D5A30] min-h-screen w-full flex flex-col gap-y-36 items-center justify-center">
-      <h1 className="text-6xl font-bold font-oswald italic text-[#7D9037] text-center">
-        Partners
+    <section className="bg-[#3D5A30] min-h-screen w-full flex flex-col gap-y-10 items-center justify-center">
+      <h1 className=" text-5xl sm:text-5xl md:text-6xl font-bold font-oswald italic text-[#7D9037] text-center pt-7">
+        A Huge Thanks To Our Rowdyhacks Partners!
       </h1>
-      <div className="FIXME">
+      {/* Render Carousel view if it is smaller than a certain width */}
+      <div className="flex justify-center items-center w-[80%] h-auto flex-wrap pb-12">
         {partnerData.partners.map((partner: Partner) => (
           <PartnerCard key={partner.name} {...partner} />
         ))}
