01a4c00d6cbb4c163dd7a3b7ed15b29bd7110c51
joshuasilva414 <joshuasilva414@gmail.com>
2024-01-30 00:09:08 -0600

    Add: Navbar Help out dropdown

--- apps/bot/bot.ts
+++ apps/bot/bot.ts
@@ -1,4 +1,10 @@
-import { Client, Collection, Events, GatewayIntentBits, EmbedBuilder } from "discord.js";
+import {
+  Client,
+  Collection,
+  Events,
+  GatewayIntentBits,
+  EmbedBuilder,
+} from "discord.js";
 import { readdirSync } from "node:fs";
 import path from "node:path";
 import { Hono } from "hono";
@@ -8,59 +14,61 @@ import c from "config";
 /* DISCORD BOT */
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
-	],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
 });
 
 client.commands = new Collection();
 
 const commandsPath = path.join(__dirname, "commands");
-const commandFiles = readdirSync(commandsPath).filter((file) => file.endsWith(".ts"));
+const commandFiles = readdirSync(commandsPath).filter((file) =>
+  file.endsWith(".ts")
+);
 for (const file of commandFiles) {
-	console.log(`[Loading Command] ${file}`);
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ("data" in command && "execute" in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(
-			`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-		);
-	}
+  console.log(`[Loading Command] ${file}`);
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
 }
 console.log(`Loaded ${client.commands.size} Commands`);
 
 client.on(Events.InteractionCreate, async (interaction) => {
-	if (!interaction.isChatInputCommand()) return;
+  if (!interaction.isChatInputCommand()) return;
 
-	const command = interaction.client.commands.get(interaction.commandName);
+  const command = interaction.client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({
-				content: "There was an error while executing this command!",
-				ephemeral: true,
-			});
-		} else {
-			await interaction.reply({
-				content: "There was an error while executing this command!",
-				ephemeral: true,
-			});
-		}
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
 });
 
 client.login(process.env.DISCORD_SECRET_TOKEN);
@@ -69,46 +77,50 @@ client.login(process.env.DISCORD_SECRET_TOKEN);
 
 const app = new Hono();
 app.get("/postMsgToServer", (h) => {
-	const internalAuthKey = h.req.query("access");
-	const serverType: string | undefined | "dev" | "prod" = h.req.query("env");
-	if (!internalAuthKey || internalAuthKey != process.env.INTERNAL_AUTH_KEY) {
-		return h.text("access denied");
-	}
-	if (!serverType || (serverType !== "dev" && serverType !== "prod")) {
-		return h.text("invalid env");
-	}
+  const internalAuthKey = h.req.query("access");
+  const serverType: string | undefined | "dev" | "prod" = h.req.query("env");
+  if (!internalAuthKey || internalAuthKey != process.env.INTERNAL_AUTH_KEY) {
+    return h.text("access denied");
+  }
+  if (!serverType || (serverType !== "dev" && serverType !== "prod")) {
+    return h.text("invalid env");
+  }
 
-	const verifyEmbed = new EmbedBuilder()
-		.setColor(0x0099ff)
-		.setTitle("Verification")
-		.setURL(c.siteUrl)
-		.setAuthor({ name: c.botName, iconURL: c.siteUrl + c.icon.md, url: c.siteUrl })
-		.setDescription(
-			`**Verify your registration for ${c.hackathonName} ${c.itteration} to gain access to the rest of the server!**\n\nClick the "verify" button below to begin the verification process.\n\u200B`
-		)
-		.setThumbnail(`${c.siteUrl}${c.icon.md}`)
-		.setFooter({
-			text: "Questions or issues? Contact an organizer :)",
-			iconURL: "https://static.acmutsa.org/Info_Simple.svg.png",
-		});
+  const verifyEmbed = new EmbedBuilder()
+    .setColor(0x0099ff)
+    .setTitle("Verification")
+    .setURL(c.siteUrl)
+    .setAuthor({
+      name: c.botName,
+      iconURL: c.siteUrl + c.icon.md,
+      url: c.siteUrl,
+    })
+    .setDescription(
+      `**Verify your registration for ${c.hackathonName} ${c.itteration} to gain access to the rest of the server!**\n\nClick the "verify" button below to begin the verification process.\n\u200B`
+    )
+    .setThumbnail(`${c.siteUrl}${c.icon.md}`)
+    .setFooter({
+      text: "Questions or issues? Contact an organizer :)",
+      iconURL: "https://static.acmutsa.org/Info_Simple.svg.png",
+    });
 
-	const channel = client.channels.cache.get(
-		serverType === "dev"
-			? (process.env.DISCORD_DEV_VERIFY_CHANNEL_ID as string)
-			: (process.env.DISCORD_PROD_VERIFY_CHANNEL_ID as string)
-	);
+  const channel = client.channels.cache.get(
+    serverType === "dev"
+      ? (process.env.DISCORD_DEV_VERIFY_CHANNEL_ID as string)
+      : (process.env.DISCORD_PROD_VERIFY_CHANNEL_ID as string)
+  );
 
-	if (!channel || !channel.isTextBased()) {
-		return h.text("Invalid channel");
-	}
+  if (!channel || !channel.isTextBased()) {
+    return h.text("Invalid channel");
+  }
 
-	channel.send({
-		embeds: [verifyEmbed],
-	});
-	return h.text(`Posted to channel!`);
+  channel.send({
+    embeds: [verifyEmbed],
+  });
+  return h.text(`Posted to channel!`);
 });
 
 serve({
-	fetch: app.fetch,
-	port: 4000,
+  fetch: app.fetch,
+  port: 4000,
 });
--- apps/web/src/components/shared/Navbar.tsx
+++ apps/web/src/components/shared/Navbar.tsx
@@ -5,47 +5,84 @@ import { Button } from "../shadcn/ui/button";
 import ProfileButton from "../dash/shared/ProfileButton";
 import { auth, currentUser } from "@clerk/nextjs";
 import NavBarLinksGrouper from "./NavBarLinksGrouper";
+import NavbarItem from "./NavbarItem";
+import NavbarDropdown from "./NavbarDropdown";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from "@/components/shadcn/ui/navigation-menu";
 
 export default async function Navbar() {
-	const user = await currentUser();
-	return (
-		<div className="fixed top-0 w-screen h-16 bg-nav z-50 border-b-border border-b">
-			<div className="w-full h-full mx-auto max-w-7xl px-5 grid grid-cols-3">
-				<div className="flex items-center justify-start gap-x-5 col-span-2">
-					<Link href={"/"} className="flex items-center gap-x-2 mr-5">
-						<Image src={c.icon.svg} alt={c.hackathonName + " Logo"} width={32} height={32} />
-						{/* <div className="bg-muted-foreground h-[45%] rotate-[25deg] w-[2px]" /> */}
-						<h2 className="font-bold tracking-tight">{c.hackathonName}</h2>
-					</Link>
+  const user = await currentUser();
+  return (
+    <div className="fixed top-0 w-screen h-16 bg-nav z-50 border-b-border border-b">
+      <div className="w-full h-full mx-auto max-w-7xl px-5 grid grid-cols-3">
+        <div className="flex items-center justify-start gap-x-5 col-span-2">
+          <Link href={"/"} className="flex items-center gap-x-2 mr-5">
+            <Image
+              src={c.icon.svg}
+              alt={c.hackathonName + " Logo"}
+              width={32}
+              height={32}
+            />
+            {/* <div className="bg-muted-foreground h-[45%] rotate-[25deg] w-[2px]" /> */}
+            <h2 className="font-bold tracking-tight">{c.hackathonName}</h2>
+          </Link>
 
-					<NavBarLinksGrouper />
-				</div>
-				<div className="items-center justify-end gap-x-4 md:flex hidden">
-					{user ? (
-						<>
-							<Link href={user.publicMetadata.registrationComplete ? "/dash" : "/register"}>
-								<Button variant={"outline"} className="bg-nav hover:bg-background">
-									{user.publicMetadata.registrationComplete ? "Dashboard" : "Complete Registration"}
-								</Button>
-							</Link>
-							<ProfileButton />
-						</>
-					) : (
-						<>
-							<Link href={"/sign-in"}>
-								<Button variant={"outline"} className="bg-nav hover:bg-background">
-									Sign In
-								</Button>
-							</Link>
-							<Link href={"/register"}>
-								<Button>Register</Button>
-							</Link>
-						</>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+          <NavBarLinksGrouper />
+        </div>
+        <div className="items-center justify-end gap-x-4 md:flex hidden">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavbarDropdown />
+              {user ? (
+                <>
+                  <Link
+                    href={
+                      user.publicMetadata.registrationComplete
+                        ? "/dash"
+                        : "/register"
+                    }
+                  >
+                    <Button
+                      variant={"outline"}
+                      className="bg-nav hover:bg-background"
+                    >
+                      {user.publicMetadata.registrationComplete
+                        ? "Dashboard"
+                        : "Complete Registration"}
+                    </Button>
+                  </Link>
+                  <ProfileButton />
+                </>
+              ) : (
+                <>
+                  <Link href={"/sign-in"}>
+                    <Button
+                      variant={"outline"}
+                      className="bg-nav hover:bg-background"
+                    >
+                      Sign In
+                    </Button>
+                  </Link>
+                  <Link href={"/register"}>
+                    <Button>Register</Button>
+                  </Link>
+                </>
+              )}
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export const runtime = "edge";
