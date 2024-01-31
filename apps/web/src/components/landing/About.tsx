a1a79aee088612c3bce336bb6ab5f777daa1bcfc
Christian Walker <christiandwalker2021@gmail.com>
2024-01-31 00:56:50 -0600

    Update about and Filler

--- apps/web/src/components/landing/About.tsx
+++ apps/web/src/components/landing/About.tsx
@@ -19,22 +19,22 @@ export default function About() {
         <div className="w-full h-full">
           <Image
             src={D1}
-            className="w-[10rem] sm:w-[11rem] md:w-[10rem] lg:w-[12rem] 2xl:w-[15rem] top-0 right-0 md:left-10 lg:left-1/2 2xl:left-12 absolute -z-[1]"
+            className="w-[9rem] sm:w-[10rem] md:w-[9rem] lg:w-[11rem] 2xl:w-[12rem] top-0 right-0 md:left-10 lg:left-1/4 2xl:left-12 absolute -z-[1]"
             alt="d1.svg"
           />
           <Image
             src={D2}
-            className="w-[11rem] sm:w-[13rem] md:w-[11rem] lg:w-[14rem] 2xl:w-[16rem] left-0 top-1/3  md:top-1/3 md:left-3/4 lg:top-[40%] lg:left-1/3 2xl:left-3/4 absolute -z-[1]"
+            className="w-[8rem] sm:w-[10rem] md:w-[9rem] lg:w-[10rem] 2xl:w-[12rem] left-4 top-1/3  md:top-1/3 md:left-3/4 lg:top-[42%] lg:left-4 2xl:left-3/4 absolute -z-[1]"
             alt="d2.svg"
           />
           <Image
             src={D3}
-            className="w-[10rem] sm:w-[12rem] md:w-[13rem]  2xl:w-[16rem] right-0 top-[45rem] sm:top-[62%] md:top-[90%] md:right-60 lg:hidden 2xl:block  2xl:top-4/5 absolute -z-[1]"
+            className="w-[9rem] sm:w-[10rem] 2xl:w-[13rem] right-0 top-[45rem] sm:top-[62%] md:top-[90%] md:right-60 lg:hidden 2xl:block  2xl:top-4/5 absolute -z-[1]"
             alt="d3.svg"
           />
           <Image
             src={D4}
-            className="w-[8rem] sm:w-[10rem] md:w-[17rem] lg:w-[10rem] 2xl:w-[12rem] bottom-10 left-12 sm:right-12 md:bottom-0 md:hidden lg:block lg:bottom-7 2xl:top-[52rem] absolute -z-[1]"
+            className="w-[7rem] sm:w-[8rem] lg:w-[9rem] 2xl:w-[11rem] bottom-10 left-12 sm:right-12 md:bottom-0 md:hidden lg:block lg:bottom-7 lg:left-1/3 2xl:top-[52rem] 2xl:left-12 absolute -z-[1]"
             alt="d4.svg"
           />
         </div>
--- apps/web/src/components/landing/Filler.tsx
+++ apps/web/src/components/landing/Filler.tsx
@@ -1,6 +1,32 @@
+import D6 from "../../../public/img/landing/d6.svg";
+import D3 from "../../../public/img/landing/d3.svg";
+import D4 from "../../../public/img/landing/d4.svg";
+import Land_Before from "../../../public/img/landing/Land_Before.svg"
+import Image from "next/image";
 export default function Filler(){
     return (
-    // Christian:  Purpose is to add a bit of fluff and affect between the hero and about section
-      <div className="w-full min-h-screen bg-[url('/img/landing/filler.svg')] bg-no-repeat bg-cover"></div>
+      // Christian:  Purpose is to add a bit of fluff and affect between the hero and about section
+      <div className=" relative w-full min-h-screen bg-[url('/img/landing/filler.svg')] bg-no-repeat bg-cover">
+        {/* <div className="flex justify-center items-center w-full min-h-screen">
+          <Image src={Land_Before} alt="Land_Before.svg" 
+          className="max-w-[70%]" 
+          />
+        </div> */}
+        <Image
+          src={D6}
+          alt="D6.svg"
+          className="w-40 sm:w-44 md:w-48 absolute right-0 top-[40%]"
+        />
+        <Image
+          src={D4}
+          alt="D6.svg"
+          className="w-24 sm:w-32 lg:w-40 absolute left-8 bottom-[30%] sm:right-20 sm:bottom-1/3"
+        />
+        <Image
+          src={D3}
+          alt="Shell asset"
+          className="w-32 sm:w-40 md:w-44 lg:w-52 absolute left-1/3 bottom-0 sm:bottom-6 lg:left-1/4"
+        />
+      </div>
     );
 }
