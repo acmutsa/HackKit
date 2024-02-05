84b1a89385dbce2f7f4eb184f3e3f2abc832bf13
Christian Walker <christiandwalker2021@gmail.com>
2024-02-04 21:09:23 -0600

    optimize gradient below About

--- apps/web/src/components/landing/FillerAboutPartners.tsx
+++ apps/web/src/components/landing/FillerAboutPartners.tsx
@@ -1,8 +1,7 @@
 export default function FillerAboutPartners(){
     return (
-      // 153 104 40
-      // 118 95 63
-      <div className="h-32 sm:h-40 md:h-60 bg-gradient-to-b from-[rgb(96,58,23)] min-[930px]:from-[rgb(153,104,30)] min-[930px]:to-[rgb(118,95,63)] to-[rgb(118,95,63)] 2xl:from-[rgb(153,104,40)] 2xl:to-[rgb(118,95,63)]"></div>
+      // [@media(min-width:953px)]:from-[rgb(130,79,32)] [@media(min-width:953px)]:to-[rgb(118,95,63)]
+      <div className="h-32 sm:h-40 md:h-[13rem]  bg-gradient-to-b from-[rgb(96,58,23)] lg:from-[rgb(130,79,32)] lg:to-[rgb(118,95,63)] xl:from-[#996828] xl:to-[rgb(118,95,63)]   to-[rgb(118,95,63)]"></div>
     );
 
 
