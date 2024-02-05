043271d2a32c7c7fd8e48da2d474126344cf4f86
Christian Walker <christiandwalker2021@gmail.com>
2024-02-04 20:10:42 -0600

    Fix partner image

--- apps/web/src/components/landing/FillerAboutPartners.tsx
+++ apps/web/src/components/landing/FillerAboutPartners.tsx
@@ -2,7 +2,7 @@ export default function FillerAboutPartners(){
     return (
       // 153 104 40
       // 118 95 63
-      <div className="h-32 sm:h-40 md:h-60 bg-gradient-to-b from-[rgb(96,58,23)] to-[rgb(118,95,63)] 2xl:from-[rgb(153,104,40)] 2xl:to-[rgb(118,95,63)]"></div>
+      <div className="h-32 sm:h-40 md:h-60 bg-gradient-to-b from-[rgb(96,58,23)] min-[930px]:from-[rgb(153,104,30)] min-[930px]:to-[rgb(118,95,63)] to-[rgb(118,95,63)] 2xl:from-[rgb(153,104,40)] 2xl:to-[rgb(118,95,63)]"></div>
     );
 
 
