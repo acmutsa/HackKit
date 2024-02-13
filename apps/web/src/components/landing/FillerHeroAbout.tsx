482daa773028f6b8eb19bece3d55eaccda0d0966
Christian Walker <christiandwalker2021@gmail.com>
2024-02-13 00:43:07 -0600

    Update FillerHero

--- apps/web/src/components/landing/FillerHeroAbout.tsx
+++ apps/web/src/components/landing/FillerHeroAbout.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Filler() {
 	return (
 		// Christian:  Purpose is to add a bit of fluff and affect between the hero and about section
-		<div className="w-screen h-[200px] lg:h-[300px] relative">
+		<div className="w-screen h-[200px] lg:h-[300px] 2xl:h-[350px] relative">
 			<Image src={"/img/landing/filler.png"} alt="" fill className="object-fill w-full h-full" />
 		</div>
 	);
