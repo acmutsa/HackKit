e8e24930e96dcff1bf8913585cb23adc1c65bf79
Christian Walker <christiandwalker2021@gmail.com>
2024-02-13 14:27:37 -0600

    Update partner colors

--- apps/web/src/components/landing/PartnerCard.tsx
+++ apps/web/src/components/landing/PartnerCard.tsx
@@ -20,19 +20,19 @@ type Partner = {
   tier: Tier;
 };
 
-const tierBorderMap = {
-  [Tier.Title]:           "w-[15rem]      sm:w-72           md:w-72       lg:w-80       2xl:w-[19rem]",
-  [Tier.Gold]:            "w-[12.75rem]   sm:w-[14.75rem]   md:w-[16rem]  lg:w-72       2xl:w-[19rem]",
-  [Tier.Silver]:          "w-[11rem]      sm:w-52           md:w-60       lg:w-[16rem]  2xl:w-[17rem] ",
-  [Tier.Bronze]:          "w-32           sm:w-40           md:w-[12rem]  lg:w-[14rem]  2xl:w-[16rem]",
-  [Tier.Rowdy_Partner]:   "w-[7rem]       sm:w-32           md:w-40       lg:w-[11rem]  2xl:w-[13rem]",
-  [Tier.In_Kind_Partner]: "w-[6rem]       sm:w-[7rem]       md:w-32       lg:w-40       2xl:w-52",
-};
+// const tierBorderMap = {
+//   [Tier.Title]:           "w-[15rem]      sm:w-72           md:w-72       lg:w-80       2xl:w-[19rem]",
+//   [Tier.Gold]:            "w-[12.75rem]   sm:w-[14.75rem]   md:w-[16rem]  lg:w-72       2xl:w-[19rem]",
+//   [Tier.Silver]:          "w-[11rem]      sm:w-52           md:w-60       lg:w-[16rem]  2xl:w-[17rem] ",
+//   [Tier.Bronze]:          "w-32           sm:w-40           md:w-[12rem]  lg:w-[14rem]  2xl:w-[16rem]",
+//   [Tier.Rowdy_Partner]:   "w-[7rem]       sm:w-32           md:w-40       lg:w-[11rem]  2xl:w-[13rem]",
+//   [Tier.In_Kind_Partner]: "w-[6rem]       sm:w-[7rem]       md:w-32       lg:w-40       2xl:w-52",
+// };
 
 const tierColorMap = {
   [Tier.Title]: "border-[3px] border-purple-700",
-  [Tier.Gold]: "border-green-700",
-  [Tier.Silver]: "border-gray-950",
+  [Tier.Gold]: "border-yellow-600",
+  [Tier.Silver]: "border-green-700",
   [Tier.Bronze]: "border-amber-800",
   [Tier.Rowdy_Partner]: "border-blue-500",
   [Tier.In_Kind_Partner]: "border-red-500",
