2cde8253822732967c69f7145e370409f8a90b76
Christian Walker <christiandwalker2021@gmail.com>
2024-02-21 03:34:02 -0600

    Update Admin icons

--- apps/web/src/app/admin/layout.tsx
+++ apps/web/src/app/admin/layout.tsx
@@ -12,6 +12,8 @@ import ProfileButton from "@/components/dash/shared/ProfileButton";
 import { Suspense } from "react";
 import ClientToast from "@/components/shared/ClientToast";
 import { redirect } from "next/navigation";
+import NavBarLinksGrouper from "@/components/shared/NavBarLinksGrouper";
+
 
 interface AdminLayoutProps {
 	children: React.ReactNode;
@@ -39,42 +41,50 @@ export default async function AdminLayout({ children }: AdminLayoutProps) {
 	}
 
 	return (
-		<>
-			<ClientToast />
-			<div className="w-full h-16 px-5 grid grid-cols-2 bg-nav fixed z-20">
-				<div className="flex items-center gap-x-4">
-					<Image src={c.icon.svg} alt={c.hackathonName + " Logo"} width={32} height={32} />
-					<div className="bg-muted-foreground h-[45%] rotate-[25deg] w-[2px]" />
-					<h2 className="font-bold tracking-tight">Admin</h2>
-				</div>
-				<div className="items-center justify-end gap-x-4 md:flex hidden">
-					<Link href={"/"}>
-						<Button variant={"outline"} className="bg-nav hover:bg-background">
-							Home
-						</Button>
-					</Link>
-					<Link href={"/guide"}>
-						<Button variant={"outline"} className="bg-nav hover:bg-background">
-							Survival Guide
-						</Button>
-					</Link>
-					<Link href={"/guide"}>
-						<Button variant={"outline"} className="bg-nav hover:bg-background">
-							Discord
-						</Button>
-					</Link>
-					<ProfileButton />
-				</div>
-				<div className="items-center justify-end gap-x-4 md:hidden flex"></div>
-			</div>
-			<div className="w-full h-12 px-5 flex bg-nav border-b-border border-b fixed mt-16 z-20">
-				{Object.entries(c.dashPaths.admin).map(([name, path]) => (
-					<DashNavItem key={name} name={name} path={path} />
-				))}
-			</div>
-			<Suspense fallback={<p>Loading...</p>}>{children}</Suspense>
-		</>
-	);
+    <>
+      <ClientToast />
+      <div className="w-full h-16 px-5 grid grid-cols-2 bg-nav fixed z-20">
+        <div className="flex items-center gap-x-4">
+          <Image
+            src={c.icon.svg}
+            alt={c.hackathonName + " Logo"}
+            width={32}
+            height={32}
+          />
+          <div className="bg-muted-foreground h-[45%] rotate-[25deg] w-[2px]" />
+          <h2 className="font-bold tracking-tight">Admin</h2>
+          <div className="hidden md:flex items-center justify-start gap-x-5 col-span-2">
+            <NavBarLinksGrouper />
+          </div>
+        </div>
+        <div className="items-center justify-end gap-x-4 md:flex hidden">
+          <Link href={"/"}>
+            <Button variant={"outline"} className="bg-nav hover:bg-background">
+              Home
+            </Button>
+          </Link>
+          <Link href={"/guide"}>
+            <Button variant={"outline"} className="bg-nav hover:bg-background">
+              Survival Guide
+            </Button>
+          </Link>
+          <Link href={"/guide"}>
+            <Button variant={"outline"} className="bg-nav hover:bg-background">
+              Discord
+            </Button>
+          </Link>
+          <ProfileButton />
+        </div>
+        <div className="items-center justify-end gap-x-4 md:hidden flex"></div>
+      </div>
+      <div className="w-full h-12 px-5 flex bg-nav border-b-border border-b fixed mt-16 z-20">
+        {Object.entries(c.dashPaths.admin).map(([name, path]) => (
+          <DashNavItem key={name} name={name} path={path} />
+        ))}
+      </div>
+      <Suspense fallback={<p>Loading...</p>}>{children}</Suspense>
+    </>
+  );
 }
 
 export const runtime = "edge";
--- apps/web/src/app/admin/page.tsx
+++ apps/web/src/app/admin/page.tsx
@@ -2,7 +2,7 @@ import { Card, CardHeader, CardContent, CardTitle } from "@/components/shadcn/ui
 import { db } from "db";
 import { sql } from "db/drizzle";
 import { users } from "db/schema";
-import { Users, UserCheck, User2 } from "lucide-react";
+import { Users, UserCheck, User2,MailCheck } from "lucide-react";
 
 export default async function Page() {
 	const totalUserCount = await db
@@ -14,51 +14,51 @@ export default async function Page() {
 	const totalCheckinCount = 0;
 
 	return (
-		<div className="w-full max-w-7xl mx-auto h-16 pt-44">
-			<div className="grid grid-cols-4 gap-x-2">
-				<Card>
-					<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-						<CardTitle className="text-sm font-medium">Registrations</CardTitle>
-						<User2 />
-					</CardHeader>
-					<CardContent>
-						<div className="text-2xl font-bold">{totalUserCount[0].count}</div>
-						{/* <p className="text-xs text-muted-foreground">+20.1% from last month</p> */}
-					</CardContent>
-				</Card>
-				<Card>
-					<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-						<CardTitle className="text-sm font-medium">Teams</CardTitle>
-						<Users />
-					</CardHeader>
-					<CardContent>
-						<div className="text-2xl font-bold">{totalTeamCount}</div>
-						{/* <p className="text-xs text-muted-foreground">+20.1% from last month</p> */}
-					</CardContent>
-				</Card>
-				<Card>
-					<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-						<CardTitle className="text-sm font-medium">RSVPs</CardTitle>
-						<UserCheck />
-					</CardHeader>
-					<CardContent>
-						<div className="text-2xl font-bold">{totalRSVPCount}</div>
-						{/* <p className="text-xs text-muted-foreground">+20.1% from last month</p> */}
-					</CardContent>
-				</Card>
-				<Card>
-					<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-						<CardTitle className="text-sm font-medium">Check-ins</CardTitle>
-						<User2 />
-					</CardHeader>
-					<CardContent>
-						<div className="text-2xl font-bold">{totalCheckinCount}</div>
-						{/* <p className="text-xs text-muted-foreground">+20.1% from last month</p> */}
-					</CardContent>
-				</Card>
-			</div>
-		</div>
-	);
+    <div className="w-full max-w-7xl mx-auto h-16 pt-44">
+      <div className="grid grid-cols-4 gap-x-2">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Registrations</CardTitle>
+            <User2 />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{totalUserCount[0].count}</div>
+            {/* <p className="text-xs text-muted-foreground">+20.1% from last month</p> */}
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Teams</CardTitle>
+            <Users />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{totalTeamCount}</div>
+            {/* <p className="text-xs text-muted-foreground">+20.1% from last month</p> */}
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">RSVPs</CardTitle>
+            <MailCheck/>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{totalRSVPCount}</div>
+            {/* <p className="text-xs text-muted-foreground">+20.1% from last month</p> */}
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Check-ins</CardTitle>
+            <UserCheck />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{totalCheckinCount}</div>
+            {/* <p className="text-xs text-muted-foreground">+20.1% from last month</p> */}
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
 
 export const runtime = "edge";
