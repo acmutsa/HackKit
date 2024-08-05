d118050199103bc9dc2655be5b01773d6ef77912
xwilson03 <xanderwilson2014@gmail.com>
2024-08-05 15:38:24 -0500

    slightly refactors checkin

--- apps/web/src/app/admin/check-in/page.tsx
+++ apps/web/src/app/admin/check-in/page.tsx
@@ -8,54 +8,53 @@ export default async function Page({
 }: {
 	searchParams: { [key: string]: string | undefined };
 }) {
-	// Returns only if search params exist
-	if (searchParams.user) {
-		const [isChecked, scanUser, hasRSVPed] = await db.transaction(
-			async (tx) => {
-				const scanUser = await tx.query.userCommonData.findFirst({
-					where: eq(userCommonData.clerkID, searchParams.user ?? "unknown"),
-				});
-				if (!scanUser) {
-					return [null, null, null];
-				}
-				const scan = await tx
-					.select({
-						checkinTimestamp: userCommonData.checkinTimestamp,
-						hasRSVPed: userCommonData.isRSVPed,
-					})
-					.from(userCommonData)
-					.where(eq(userCommonData.clerkID, searchParams.user!));
-				if (scan) {
-					return [scan[0].checkinTimestamp != null, scanUser, scan[0].hasRSVPed];
-				} else {
-					return [null, scanUser, null];
-				}
-			},
-		);
-
-		return (
-			<div>
-				<CheckinScanner
-					checkedIn={isChecked}
-					hasScanned={true}
-					scanUser={scanUser}
-					hasRSVP={hasRSVPed}
-				/>
-			</div>
-		);
+    // Check for missing searchParams
+	if (!searchParams.user) {
+        return (
+            <div>
+                <CheckinScanner
+                    hasScanned={false}
+                    checkedIn={null}
+                    scanUser={null}
+                    hasRSVP={null}
+                />
+            </div>
+        );
 	}
 
-	// Fall through case
-	return (
-		<div>
-			<CheckinScanner
-				hasScanned={false}
-				checkedIn={null}
-				scanUser={null}
-				hasRSVP={null}
-			/>
-		</div>
-	);
+    const [isChecked, scanUser, hasRSVPed] = await db.transaction(
+        async (tx) => {
+            const scanUser = await tx.query.userCommonData.findFirst({
+                where: eq(userCommonData.clerkID, searchParams.user ?? "unknown"),
+            });
+            if (!scanUser) {
+                return [null, null, null];
+            }
+            const scan = await tx
+                .select({
+                    checkinTimestamp: userCommonData.checkinTimestamp,
+                    hasRSVPed: userCommonData.isRSVPed,
+                })
+                .from(userCommonData)
+                .where(eq(userCommonData.clerkID, searchParams.user!));
+            if (scan) {
+                return [scan[0].checkinTimestamp != null, scanUser, scan[0].hasRSVPed];
+            } else {
+                return [null, scanUser, null];
+            }
+        },
+    );
+
+    return (
+        <div>
+            <CheckinScanner
+                checkedIn={isChecked}
+                hasScanned={true}
+                scanUser={scanUser}
+                hasRSVP={hasRSVPed}
+            />
+        </div>
+    );
 }
 
 export const runtime = "edge";
