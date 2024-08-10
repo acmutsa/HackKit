d6ab8f68b04b284f3046aef2542d890da9240520
xwilson03 <xanderwilson2014@gmail.com>
2024-08-10 13:18:16 -0500

    adds prepared getHacker function

--- packages/db/functions.ts
+++ packages/db/functions.ts
@@ -1,12 +1,22 @@
 import { db, eq, sql } from ".";
 import { userCommonData } from "./schema";
-import { User } from "./types";
+import { Hacker, User } from "./types";
 
 const _getUser = db.query.userCommonData
     .findFirst({
         where: eq(userCommonData.clerkID, sql.placeholder('_clerkID')),
     }).prepare("getUser");
 
-export function getUser(clerkID: string) : Promise<User | undefined>{
+export function getUser(clerkID: string) : Promise<User | undefined> {
     return _getUser.execute({_clerkID: clerkID});
+}
+
+const _getHacker = db.query.userCommonData
+    .findFirst({
+        where: eq(userCommonData.clerkID, sql.placeholder('_clerkID')),
+        with: { hackerData: { with: { team: true } } }
+    }).prepare("getHacker");
+
+export function getHacker(clerkID: string) : Promise<Hacker | undefined> {
+    return _getHacker.execute({_clerkID: clerkID});
 }
