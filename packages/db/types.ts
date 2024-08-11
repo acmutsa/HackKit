33655d92d48fc0362cff9fbe4c0969d55c79339a
xwilson03 <xanderwilson2014@gmail.com>
2024-08-11 01:50:40 -0500

    converts types to interfaces for intellisense

--- packages/db/types.ts
+++ packages/db/types.ts
@@ -1,11 +1,12 @@
+import { InferSelectModel } from "drizzle-orm";
 import { userCommonData, userHackerData, teams, scans, events } from "./schema";
 
-export type Scan = typeof scans.$inferSelect;
-export type User = typeof userCommonData.$inferSelect;
-export type Event = typeof events.$inferSelect;
+export interface Scan  extends InferSelectModel<typeof scans> {};
+export interface User  extends InferSelectModel<typeof userCommonData> {};
+export interface Event extends InferSelectModel<typeof events> {};
 
 export interface Hacker extends User {
-	hackerData: typeof userHackerData.$inferSelect & {
-		team: typeof teams.$inferSelect | null;
+    hackerData: typeof userHackerData.$inferSelect & {
+        team: typeof teams.$inferSelect | null;
 	};
 }
