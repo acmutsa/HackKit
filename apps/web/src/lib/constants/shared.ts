714eeec656159fee1316570da22e8ce17a95dd6c
Christian Walker <christiandwalker2021@gmail.com>
2024-08-17 18:18:36 -0400

    Update for formatting

--- apps/web/src/lib/constants/shared.ts
+++ apps/web/src/lib/constants/shared.ts
@@ -1,9 +1,6 @@
-import c from "config"
+import c from "config";
 
-const {
-  hackathonName,
-  itteration
-} = c;
+const { hackathonName, itteration } = c;
 
 export const ONE_HOUR_IN_MILLISECONDS = 3600000;
 export const VERCEL_IP_TIMEZONE_HEADER_KEY = "x-vercel-ip-timezone";
