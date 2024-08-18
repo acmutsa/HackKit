e88a3b4ccd80348499c245d0f38046ecbc5079a4
Christian Walker <christiandwalker2021@gmail.com>
2024-08-18 02:41:50 -0400

    Move configurable constant

--- apps/web/src/lib/constants/shared.ts
+++ apps/web/src/lib/constants/shared.ts
@@ -1,5 +1,2 @@
-import c from "config";
-
 export const ONE_HOUR_IN_MILLISECONDS = 3600000;
 export const VERCEL_IP_TIMEZONE_HEADER_KEY = "x-vercel-ip-timezone";
-export const BUCKET_RESUME_URL = `${c.hackathonName}/${c.itteration}/resume`;
