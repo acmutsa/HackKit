2f923314f5bc12466611dca33dd8fffd7ae89ca7
xwilson03 <xanderwilson2014@gmail.com>
2024-08-18 03:31:30 -0500

    restructures helper functions

--- packages/db/functions/hacker.ts
+++ packages/db/functions/hacker.ts
@@ -1,19 +1,7 @@
-import { db, eq, sql } from ".";
-import { userCommonData } from "./schema";
-import { Hacker, User } from "./types";
+import { db, eq, sql } from "..";
+import { userCommonData } from "../schema";
+import { Hacker } from "../types";
 
-// USER
-
-const _getUser = db.query.userCommonData
-    .findFirst({
-        where: eq(userCommonData.clerkID, sql.placeholder('_clerkID')),
-    }).prepare("getUser");
-
-export function getUser(clerkID: string) : Promise<User | undefined> {
-    return _getUser.execute({_clerkID: clerkID});
-}
-
-// HACKER
 
 const _getHackerWithTeam = db.query.userCommonData
     .findFirst({
---
+++ packages/db/functions/user.ts
@@ -0,0 +1,13 @@
+import { db, eq, sql } from "..";
+import { userCommonData } from "../schema";
+import { User } from "../types";
+
+
+const _getUser = db.query.userCommonData
+    .findFirst({
+        where: eq(userCommonData.clerkID, sql.placeholder('_clerkID')),
+    }).prepare("getUser");
+
+export function getUser(clerkID: string) : Promise<User | undefined> {
+    return _getUser.execute({_clerkID: clerkID});
+}
