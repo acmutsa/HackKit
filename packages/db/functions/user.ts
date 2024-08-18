6542e91ed9a781dbf85734f018698dc7c2fb5d54
xwilson03 <xanderwilson2014@gmail.com>
2024-08-18 03:48:06 -0500

    adds user search by tag

--- packages/db/functions/user.ts
+++ packages/db/functions/user.ts
@@ -2,6 +2,7 @@ import { db, eq, sql } from "..";
 import { userCommonData } from "../schema";
 import { User } from "../types";
 
+// ID
 
 const _getUser = db.query.userCommonData
     .findFirst({
@@ -10,4 +11,15 @@ const _getUser = db.query.userCommonData
 
 export function getUser(clerkID: string) : Promise<User | undefined> {
     return _getUser.execute({_clerkID: clerkID});
+}
+
+// Tag
+
+const _getUserByTag = db.query.userCommonData
+    .findFirst({
+        where: eq(userCommonData.hackerTag, sql.placeholder('_hackerTag')),
+    }).prepare("getUserByTag");
+
+export function getUserByTag(hackerTag: string) : Promise<User | undefined> {
+    return _getUserByTag.execute({_hackerTag: hackerTag});
 }
