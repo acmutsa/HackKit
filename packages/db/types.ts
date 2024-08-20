3a7bf22a54a06a088e9a4f1fd7dc2e9e279214fa
xwilson03 <xanderwilson2014@gmail.com>
2024-08-19 21:46:49 -0500

    adds team interface definition

--- packages/db/types.ts
+++ packages/db/types.ts
@@ -3,10 +3,11 @@ import { userCommonData, userHackerData, teams, scans, events } from "./schema";
 
 export interface Scan  extends InferSelectModel<typeof scans> {};
 export interface User  extends InferSelectModel<typeof userCommonData> {};
+export interface Team  extends InferSelectModel<typeof teams> {};
 export interface Event extends InferSelectModel<typeof events> {};
 
 export interface Hacker extends User {
     hackerData: typeof userHackerData.$inferSelect & {
-        team?: typeof teams.$inferSelect | null;
+        team?: Team | null;
 	};
 }
