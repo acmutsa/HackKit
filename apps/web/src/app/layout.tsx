4b106e5b933ee7624b2cdb66e9546d03e14d24d8
Edmund Dong <12579441+EdmundDong@users.noreply.github.com>
2024-08-20 18:17:18 -0700

    Add meta tags

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Analytics } from '@vercel/analytics/react';
 import { defaultTheme } from 'config';
 import Script from 'next/script';
 import { Alata } from 'next/font/google';
+import meta from '../../public/img/meta.png'
 
 const font = Alata({
   subsets: ['latin'],
@@ -20,6 +21,31 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          {/* HTML Meta Tags */}
+          <title>sunhacks - September 2024</title>
+          <meta name="description" content="Join us for sunhacks on September 28 at the SDFC, ASU Tempe!" />
+
+          {/* Google / Search Engine Tags */}
+          <meta itemProp="name" content="sunhacks - September 2024" />
+          <meta itemProp="description" content="Join us for sunhacks on September 28 at the SDFC, ASU Tempe!" />
+          <meta itemProp="image" content={meta.src} />
+
+          {/* Facebook Meta Tags */}
+          <meta property="og:url" content="https://sunhacks.io/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="sunhacks - September 2024" />
+          <meta property="og:description" content="Join us for sunhacks on September 28 at the SDFC, ASU Tempe!" />
+          <meta property="og:image" content={meta.src} />
+
+          {/* Twitter Meta Tags */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="sunhacks - September 2024" />
+          <meta name="twitter:description" content="Join us for sunhacks on September 28 at the SDFC, ASU Tempe!" />
+          <meta name="twitter:image" content={meta.src} />
+
+          {/* Meta Tags Generated via https://heymeta.com */}
+        </head>
         <body className={`${theme === 'dark' ? 'dark' : ''} ${font.className}`}>
           {children}
           <Analytics />
