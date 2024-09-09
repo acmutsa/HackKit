f01fe17cdb04fcd7ef6bdb49d3d7e314a49d0052
Saharsh Goenka <saharsh.goenka3@gmail.com>
2024-09-08 19:48:24 -0700

    Remove "Priority" from registration button

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -81,7 +81,7 @@ export default function Home() {
               <Link href="/register">
                 <button
                     className="text-lg text-white text-center mt-4 bg-black hover:bg-white hover:text-black py-2 px-4 rounded-md">
-                  Priority Registration Open
+                  Registration Open
                 </button>
               </Link>
               <a
