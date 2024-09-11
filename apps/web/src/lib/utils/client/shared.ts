c1aa7ec655565c5d233ceec085f08c5df097d91f
Christian Walker <christiandwalker2021@gmail.com>
2024-09-10 23:50:29 -0500

    removes unused constant

--- apps/web/src/lib/utils/client/shared.ts
+++ apps/web/src/lib/utils/client/shared.ts
@@ -1,4 +1,3 @@
-import { ONE_HOUR_IN_MILLISECONDS } from "@/lib/constants";
 
 export function getClientTimeZone(vercelIPTimeZone: string | null) {
 	return vercelIPTimeZone ?? Intl.DateTimeFormat().resolvedOptions().timeZone;
