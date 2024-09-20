c921ad6eb9b6398c808c76cdeb9fbc8c8afaa650
Christian Walker <christiandwalker2021@gmail.com>
2024-09-20 03:00:42 -0500

    creates db zod

---
+++ packages/db/zod.ts
@@ -0,0 +1,10 @@
+import { userCommonData,userHackerData } from "./schema";
+import { createInsertSchema } from "drizzle-zod";
+
+export const userCommonDataInsertSchema = createInsertSchema(userCommonData);
+export const userHackerDataInsertSchema = createInsertSchema(userHackerData);
+
+export const userWithHackerDataInsertSchema = userCommonDataInsertSchema
+	.merge(
+    userHackerDataInsertSchema
+  );
