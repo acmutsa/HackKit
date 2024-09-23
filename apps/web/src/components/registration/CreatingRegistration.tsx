107a27bb937731aa383a3cbb9734c33c0196993b
Christian Walker <christiandwalker2021@gmail.com>
2024-09-23 03:33:59 -0500

    updates loading for registration

--- apps/web/src/components/registration/CreatingRegistration.tsx
+++ apps/web/src/components/registration/CreatingRegistration.tsx
@@ -1,10 +1,10 @@
-import { Bars } from "react-loader-spinner";
+import { Bars,ThreeDots,ThreeCircles } from "react-loader-spinner";
 
 export default function CreatingRegistration() {
 	return (
 		<main className="absolute bottom-0 left-0 z-10 flex min-h-screen w-screen flex-col items-center justify-center gap-y-20 bg-background">
-			<h1 className="text-3xl font-black">Creating Your Registration!</h1>
-			<Bars
+			<h1 className="text-3xl font-black text-center w-full">Creating Your Registration...</h1>
+			<ThreeCircles
 				height="80"
 				width="80"
 				color="hsl(var(--primary))"
