0dcde1850d9dc22550bfcc6e7d37eba846197622
Christian Walker <christiandwalker2021@gmail.com>
2024-09-23 03:34:22 -0500

    adds user id to props

--- apps/web/src/app/register/page.tsx
+++ apps/web/src/app/register/page.tsx
@@ -52,6 +52,7 @@ export default async function Page() {
 							defaultEmail={
 								user.emailAddresses[0]?.emailAddress || ""
 							}
+							userId={userId}
 						/>
 					</div>
 				</main>
