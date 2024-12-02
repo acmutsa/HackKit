6eaf88c9e77b64023d6b6d57d9122b3a5cf0b123
Christian Walker <christiandwalker2021@gmail.com>
2024-12-02 14:45:58 -0600

    first init

--- apps/web/src/components/admin/users/UserColumns.tsx
+++ apps/web/src/components/admin/users/UserColumns.tsx
@@ -6,29 +6,56 @@ import { createSelectSchema } from "drizzle-zod";
 import { userCommonData } from "db/schema";
 import Link from "next/link";
 import { Button } from "@/components/shadcn/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "../../shadcn/ui/dropdown-menu";
+import { MoreHorizontal,ArrowUpDown } from "lucide-react";
+import type { Column,Row } from "@tanstack/react-table";
 
 const userValidator = createSelectSchema(userCommonData);
 
+// default fuzzy search and add filters by each column if possible 
 export type userValidatorType = Pick<
 	z.infer<typeof userValidator>,
 	| "clerkID"
 	| "signupTime"
 	| "firstName"
 	| "lastName"
-	| "hackerTag"
 	| "email"
 	| "role"
+	| 'isRSVPed'
+	| 'hackerTag'
+	| 'checkinTimestamp'
 >;
+
+type UserColumnType = Column<userValidatorType, unknown>;
 
 export const columns: ColumnDef<userValidatorType>[] = [
 	{
 		accessorKey: "firstName",
-		header: "Name",
+		header: ({column}) => {
+			return (
+				<SortColumnButton name="Name" column={column} />
+			)
+		},
 		cell: ({ row }) => `${row.original.firstName} ${row.original.lastName}`,
+		filterFn: (row, _columnId, filterValue) => {
+			return row.original.firstName.toLocaleLowerCase().includes(filterValue.toLocaleLowerCase()) || row.original.lastName.toLocaleLowerCase().includes(filterValue.toLocaleLowerCase());
+		}
 	},
 	{
 		accessorKey: "email",
-		header: "Email",
+		header: ({ column }) => {
+      return (
+        <SortColumnButton name="Email" column={column} />
+      )
+    },
 	},
 	{
 		accessorKey: "hackerTag",
@@ -36,8 +63,33 @@ export const columns: ColumnDef<userValidatorType>[] = [
 		cell: ({ row }) => `@${row.original.hackerTag}`,
 	},
 	{
-		accessorKey: "clerkID",
-		header: "Account ID",
+		accessorKey: "isRSVPed",
+		header: "RSVP Status",
+		cell: ({ row }) => (row.original.isRSVPed ? "RSVPed" : "Not RSVPed"),
+	},
+	{
+		accessorKey: "checkinTimestamp",
+		header: ({ column }) => {
+      return (
+        <SortColumnButton name="Checkin Time" column={column} />
+      )
+    },
+		cell: ({ row }) => (
+			<span suppressHydrationWarning={true}>
+				{row.original.checkinTimestamp
+					? new Date(
+							row.original.checkinTimestamp,
+						).toLocaleDateString() +
+						" " +
+						new Date(
+							row.original.checkinTimestamp,
+						).toLocaleTimeString("en-US", {
+							hour: "2-digit",
+							minute: "2-digit",
+						})
+					: "Not Checked In"}
+			</span>
+		),
 	},
 	{
 		accessorKey: "role",
@@ -45,7 +97,11 @@ export const columns: ColumnDef<userValidatorType>[] = [
 	},
 	{
 		accessorKey: "signupTime",
-		header: "Signup Date",
+		header: ({ column }) => {
+      return (
+        <SortColumnButton name="Signup Time" column={column} />
+      )
+    },
 		cell: ({ row }) => (
 			<span suppressHydrationWarning={true}>
 				{new Date(row.original.signupTime).toLocaleDateString() + " "}
@@ -57,12 +113,62 @@ export const columns: ColumnDef<userValidatorType>[] = [
 		),
 	},
 	{
-		accessorKey: "clerkID2",
-		header: "View",
-		cell: ({ row }) => (
-			<Link href={`/admin/users/${row.original.clerkID}`}>
-				<Button>View</Button>
-			</Link>
-		),
+		id: "actions",
+		enableHiding: false,
+		cell: ({ row }) => {
+			return <UserDropDownActions row={row} />;
+		},
 	},
 ];
+
+function UserDropDownActions({row}:{row:Row<userValidatorType>}) {
+	const user = row.original;
+
+			return (
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost" className="h-8 w-8 p-0">
+							<span className="sr-only">Open menu</span>
+							<MoreHorizontal size={20} />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuItem>
+							<Link href={`/admin/users/${user.clerkID}`}>
+								View User
+							</Link>
+						</DropdownMenuItem>
+						<DropdownMenuItem
+							onClick={() =>
+								navigator.clipboard.writeText(user.clerkID)
+							}
+							className="cursor-pointer"
+						>
+							Copy Clerk ID
+						</DropdownMenuItem>
+						<DropdownMenuItem>
+							<Link
+								href={`mailto:${user.email}`}
+								target="_blank"
+								prefetch={false}
+							>
+								Email User
+							</Link>
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			);
+		}
+
+function SortColumnButton({name,column}:{name:string,column:UserColumnType}) {
+	return (
+		<Button
+			variant="ghost"
+			onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+		>
+			{name}
+			<ArrowUpDown className="ml-2 h-4 w-4" />
+		</Button>
+	)
+}
+
--- apps/web/src/components/admin/users/UserDataTable.tsx
+++ apps/web/src/components/admin/users/UserDataTable.tsx
@@ -5,6 +5,11 @@ import {
 	flexRender,
 	getCoreRowModel,
 	useReactTable,
+	SortingState,
+	getSortedRowModel,
+	getPaginationRowModel,
+	ColumnFiltersState,
+	getFilteredRowModel
 } from "@tanstack/react-table";
 
 import {
@@ -15,9 +20,9 @@ import {
 	TableHeader,
 	TableRow,
 } from "@/components/shadcn/ui/table";
-
-import { useCallback } from "react";
-import { useRouter } from "next/navigation";
+import { Input } from "@/components/shadcn/ui/input";
+import { Button } from "@/components/shadcn/ui/button";
+import { useEffect, useState } from "react";
 
 interface DataTableProps<TData, TValue> {
 	columns: ColumnDef<TData, TValue>[];
@@ -28,68 +33,125 @@ export function DataTable<TData, TValue>({
 	columns,
 	data,
 }: DataTableProps<TData, TValue>) {
-	const formatTrProps = (state = {}) => {
-		console.log("qua");
-		return { onClick: () => console.log("state", state) };
-	};
+
+	const [sorting,setSorting] = useState<SortingState>([]);
+	const [columnFilters, setColumnFilters] =
+		useState<ColumnFiltersState>([]);
 
 	const table = useReactTable({
 		data,
 		columns,
 		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		onSortingChange:setSorting,
+		getSortedRowModel: getSortedRowModel(),
+		onColumnFiltersChange: setColumnFilters,
+		getFilteredRowModel: getFilteredRowModel(),
+		state:{
+			sorting,
+			columnFilters
+		}
 	});
 
+	useEffect(()=>{
+		console.log("column filters",columnFilters);
+	},[columnFilters]);
+
 	return (
-		<div className="rounded-md border">
-			<Table>
-				<TableHeader>
-					{table.getHeaderGroups().map((headerGroup) => (
-						<TableRow key={headerGroup.id}>
-							{headerGroup.headers.map((header) => {
-								return (
-									<TableHead key={header.id}>
-										{header.isPlaceholder
-											? null
-											: flexRender(
-													header.column.columnDef
-														.header,
-													header.getContext(),
-												)}
-									</TableHead>
-								);
-							})}
-						</TableRow>
-					))}
-				</TableHeader>
-				<TableBody>
-					{table.getRowModel().rows?.length ? (
-						table.getRowModel().rows.map((row) => (
-							<TableRow
-								key={row.id}
-								data-state={row.getIsSelected() && "selected"}
-							>
-								{row.getVisibleCells().map((cell) => (
-									<TableCell key={cell.id}>
-										{flexRender(
-											cell.column.columnDef.cell,
-											cell.getContext(),
-										)}
-									</TableCell>
-								))}
+		<div>
+			<div className="rounded-md border">
+				<Input
+					placeholder="Filter users..."
+					value={
+						(table
+							.getColumn("firstName")
+							?.getFilterValue() as string) ?? ""
+					}
+					onChange={(event) => {
+						table
+							.getColumn("firstName")
+							?.setFilterValue(event.target.value);
+						table
+							.getColumn("email")
+							?.setFilterValue(event.target.value);
+						// table
+						// 	.getColumn("hackerTag")
+						// 	?.setFilterValue(event.target.value);
+					}
+						
+					}
+					className="max-w-sm"
+				/>
+				<Table>
+					<TableHeader>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<TableRow key={headerGroup.id}>
+								{headerGroup.headers.map((header) => {
+									return (
+										<TableHead key={header.id}>
+											{header.isPlaceholder
+												? null
+												: flexRender(
+														header.column.columnDef
+															.header,
+														header.getContext(),
+													)}
+										</TableHead>
+									);
+								})}
 							</TableRow>
-						))
-					) : (
-						<TableRow>
-							<TableCell
-								colSpan={columns.length}
-								className="h-24 text-center"
-							>
-								No results.
-							</TableCell>
-						</TableRow>
-					)}
-				</TableBody>
-			</Table>
+						))}
+					</TableHeader>
+					<TableBody>
+						{table.getRowModel().rows?.length ? (
+							table.getRowModel().rows.map((row) => (
+								<TableRow
+									key={row.id}
+									data-state={
+										row.getIsSelected() && "selected"
+									}
+								>
+									{row.getVisibleCells().map((cell) => (
+										<TableCell key={cell.id}>
+											{flexRender(
+												cell.column.columnDef.cell,
+												cell.getContext(),
+											)}
+										</TableCell>
+									))}
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								<TableCell
+									colSpan={columns.length}
+									className="h-24 text-center"
+								>
+									No results.
+								</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+			</div>
+			<div className="flex items-center justify-end space-x-2 py-4">
+				<Button
+					variant="outline"
+					size="sm"
+					onClick={() => table.previousPage()}
+					disabled={!table.getCanPreviousPage()}
+				>
+					Previous
+				</Button>
+				<Button
+					variant="outline"
+					size="sm"
+					onClick={() => table.nextPage()}
+					disabled={!table.getCanNextPage()}
+				>
+					Next
+				</Button>
+			</div>
 		</div>
 	);
 }
