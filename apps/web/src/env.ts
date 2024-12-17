c963fb0a9b3a93a95f60ea6093068daf68ce9e96
Carter LaVigne <carterlavigne3@gmail.com>
2024-12-17 13:11:13 -0600

    prettier check

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -3,32 +3,39 @@ import { z } from "zod";
 
 // Helper function to handle empty strings as undefined
 const stringOrUndefined = z
-  .string()
-  .refine((val) => val !== "", { message: "Value cannot be an empty string" })
-  .transform((val) => (val === "" ? undefined : val));
+	.string()
+	.refine((val) => val !== "", { message: "Value cannot be an empty string" })
+	.transform((val) => (val === "" ? undefined : val));
 
 export const env = createEnv({
-  server: {
-    CLERK_SECRET_KEY: stringOrUndefined,
-    AWS_SES_ACCESS_KEY: stringOrUndefined,
-    AWS_SES_SECRET_ACCESS_KEY: stringOrUndefined,
-    AWS_REGION: stringOrUndefined,
-    AWS_SES_EMAIL_FROM: stringOrUndefined,
-    INTERNAL_AUTH_KEY: z.string().min(64, { message: "INTERNAL_AUTH_KEY must be at least 64 characters" }),
-    BOT_API_URL: stringOrUndefined,
-    NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
-  },
-  client: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: stringOrUndefined,
-  },
-  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
-  //   runtimeEnv: {
-  //     DATABASE_URL: process.env.DATABASE_URL,
-  //     OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
-  //     NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
-  //   },
-  //   For Next.js >= 13.4.4, you only need to destructure client variables:
-  experimental__runtimeEnv: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-  },
+	server: {
+		CLERK_SECRET_KEY: stringOrUndefined,
+		AWS_SES_ACCESS_KEY: stringOrUndefined,
+		AWS_SES_SECRET_ACCESS_KEY: stringOrUndefined,
+		AWS_REGION: stringOrUndefined,
+		AWS_SES_EMAIL_FROM: stringOrUndefined,
+		INTERNAL_AUTH_KEY: z
+			.string()
+			.min(64, {
+				message: "INTERNAL_AUTH_KEY must be at least 64 characters",
+			}),
+		BOT_API_URL: stringOrUndefined,
+		NODE_ENV: z
+			.enum(["development", "test", "production"])
+			.default("development"),
+	},
+	client: {
+		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: stringOrUndefined,
+	},
+	// If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
+	//   runtimeEnv: {
+	//     DATABASE_URL: process.env.DATABASE_URL,
+	//     OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
+	//     NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+	//   },
+	//   For Next.js >= 13.4.4, you only need to destructure client variables:
+	experimental__runtimeEnv: {
+		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+			process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+	},
 });
