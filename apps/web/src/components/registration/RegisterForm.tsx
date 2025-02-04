3cb67d7f36558295b8df1434a20132d2896d7f87
Christian Walker <christiandwalker2021@gmail.com>
2025-02-03 18:01:00 -0600

    make redirect time smaller

--- apps/web/src/components/registration/RegisterForm.tsx
+++ apps/web/src/components/registration/RegisterForm.tsx
@@ -238,7 +238,7 @@ export default function RegisterForm({
 					);
 					setTimeout(() => {
 						router.push("/dash");
-					}, 1000);
+					}, 600);
 				} else {
 					setIsLoading(false);
 					console.error("onSuccess Error data:", data);
