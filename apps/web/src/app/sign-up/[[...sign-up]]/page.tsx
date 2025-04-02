cb98dffb6a9d5f5b3b1aa0aafb01140e251f9596
joshuasilva414 <joshuasilva414@gmail.com>
2025-04-01 23:02:15 -0500

    run prettier

--- apps/web/src/app/sign-up/[[...sign-up]]/page.tsx
+++ apps/web/src/app/sign-up/[[...sign-up]]/page.tsx
@@ -17,9 +17,7 @@ export default async function Page() {
 	if (parseRedisBoolean(defaultRegistrationEnabled, true) === true) {
 		return (
 			<main className="flex min-h-screen items-center justify-center">
-				<SignUp
-					fallbackRedirectUrl={"/register"}
-				/>
+				<SignUp fallbackRedirectUrl={"/register"} />
 			</main>
 		);
 	}
