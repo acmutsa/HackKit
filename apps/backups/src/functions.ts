41466ae1b6289c3594d09356454044674e2f18b1
Christian Walker <christiandwalker2021@gmail.com>
2025-07-20 18:34:26 -0600

    formatter

--- apps/backups/src/functions.ts
+++ apps/backups/src/functions.ts
@@ -1,6 +1,5 @@
-import { env } from "./env"
+import { env } from "./env";
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-
 
 const S3 = new S3Client({
 	region: "auto",
@@ -11,48 +10,53 @@ const S3 = new S3Client({
 	},
 });
 
-async function getDatabaseDump(databseName:string, organizationSlug:string){
-  const res = await fetch(`https://${databseName}-${organizationSlug}.turso.io/dump`,{
-    method:"GET",
-    headers:new Headers({
-      'Authorization': `Bearer ${env.BACKUPS_DB_BEARER}`,
-    })
-  })
+async function getDatabaseDump(databseName: string, organizationSlug: string) {
+	const res = await fetch(
+		`https://${databseName}-${organizationSlug}.turso.io/dump`,
+		{
+			method: "GET",
+			headers: new Headers({
+				Authorization: `Bearer ${env.BACKUPS_DB_BEARER}`,
+			}),
+		},
+	);
 
-  if (!res.ok) {
-    throw new Error(`Failed to get database dump: ${res.status} ${res.statusText}`);
-  }
+	if (!res.ok) {
+		throw new Error(
+			`Failed to get database dump: ${res.status} ${res.statusText}`,
+		);
+	}
 
-  return res.text();
+	return res.text();
 }
 
-
-async function uploadToS3(fileName:string, dumpFile:string){
-
-  const cmd = new PutObjectCommand({
+async function uploadToS3(fileName: string, dumpFile: string) {
+	const cmd = new PutObjectCommand({
 		Key: fileName,
 		Bucket: env.BACKUPS_BUCKET_NAME,
-		Body: Buffer.from(dumpFile, 'utf-8'),
-  });
-  console.log("Uploading to S3", fileName, env.BACKUPS_BUCKET_NAME);
-  return S3.send(cmd);
+		Body: Buffer.from(dumpFile, "utf-8"),
+	});
+	console.log("Uploading to S3", fileName, env.BACKUPS_BUCKET_NAME);
+	return S3.send(cmd);
 }
 
-async function doBackup(){
-  const backupData = await getDatabaseDump(env.BACKUPS_DATABSE_NAME, env.BACKUPS_ORGANIZATION_SLUG);
-  const date = new Date().toISOString();
-  const timestamp = date.replace(/[:.]+/g, "-");
-  const fileName = `${env.BACKUPS_DATABSE_NAME}-${env.BACKUPS_ORGANIZATION_SLUG}-${timestamp}.sql`;
-  const res = await uploadToS3(fileName, backupData);
+async function doBackup() {
+	const backupData = await getDatabaseDump(
+		env.BACKUPS_DATABSE_NAME,
+		env.BACKUPS_ORGANIZATION_SLUG,
+	);
+	const date = new Date().toISOString();
+	const timestamp = date.replace(/[:.]+/g, "-");
+	const fileName = `${env.BACKUPS_DATABSE_NAME}-${env.BACKUPS_ORGANIZATION_SLUG}-${timestamp}.sql`;
+	const res = await uploadToS3(fileName, backupData);
 
-  if (res.$metadata.httpStatusCode !== 200) {
+	if (res.$metadata.httpStatusCode !== 200) {
 		throw new Error(
 			`Failed to upload file to S3: ${res.$metadata.httpStatusCode}`,
 		);
-  }
-  console.log(`File uploaded to S3: ${fileName}`);
-  return true; 
+	}
+	console.log(`File uploaded to S3: ${fileName}`);
+	return true;
 }
 
-
-export {doBackup, getDatabaseDump, uploadToS3};
+export { doBackup, getDatabaseDump, uploadToS3 };
