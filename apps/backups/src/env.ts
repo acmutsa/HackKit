bfbc91b678c62ecc1890422d3693b3513a483c41
Christian Walker <christiandwalker2021@gmail.com>
2025-07-20 22:04:36 -0600

    updates env test

--- apps/backups/src/env.ts
+++ apps/backups/src/env.ts
@@ -1,13 +1,13 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
-
 export const env = createEnv({
 	server: {
 		BACKUPS_SECRET_KEY: z.string(),
 		BACKUPS_DATABSE_NAME: z.string(),
 		BACKUPS_ORGANIZATION_SLUG: z.string(),
 		BACKUPS_CLOUDFLARE_ACCOUNT_ID: z.string({
-			description:"Account ID for the Cloudflare account. Note that this must match what you are "
+			description:
+				"Account ID for the Cloudflare account. Note that this must match what you are ",
 		}),
 		BACKUPS_BUCKET_ACCESS_KEY_ID: z.string(),
 		BACKUPS_BUCKET_SECRET_ACCESS_KEY: z.string(),
@@ -17,8 +17,19 @@ export const env = createEnv({
 		}),
 		BACKUPS_BUCKET_NAME: z.string(),
 	},
-	skipValidation:true,
-
+	onValidationError: (issues) => {
+		console.log("all process variables:", process.env)
+		console.error("❌ Invalid environment variables:", issues);
+		throw new Error("Invalid environment variables");
+	},
+	// Called when server variables are accessed on the client.
+	onInvalidAccess: (variable: string) => {
+		console.log(`❌ Attempted to access server-side environment variable "${variable}" on the client.`);
+		throw new Error(
+			"❌ Attempted to access a server-side environment variable on the client",
+		);
+	},
+	// skipValidation: true,
 	runtimeEnv: process.env,
 	emptyStringAsUndefined: true,
 });
