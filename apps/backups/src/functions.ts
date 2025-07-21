a4d13351234d198925eec815ae99743699615739
Christian Walker <christiandwalker2021@gmail.com>
2025-07-20 21:47:45 -0600

    removes debugs

--- apps/backups/src/functions.ts
+++ apps/backups/src/functions.ts
@@ -10,9 +10,6 @@ const S3 = new S3Client({
 	},
 });
 
-console.log("env test", env.BACKUPS_BUCKET_NAME);
-console.log("env test", env.BACKUPS_DATABSE_NAME);
-
 async function getDatabaseDump(databseName: string, organizationSlug: string) {
 	const res = await fetch(
 		`https://${databseName}-${organizationSlug}.turso.io/dump`,
