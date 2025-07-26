ba7d230a9f07ab1180f5288a4919751179b1d6ba
Christian Walker <christiandwalker2021@gmail.com>
2025-07-26 12:56:41 -0600

    updates descriptions

--- apps/backups/src/env.ts
+++ apps/backups/src/env.ts
@@ -2,18 +2,21 @@ import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 export const env = createEnv({
 	server: {
-		BACKUPS_SECRET_KEY: z.string(),
+		BACKUPS_SECRET_KEY: z.string({
+			description:
+				"This is a secret key used to sign and verify requests. It should be kept secret and not shared with anyone.",
+		}),
 		BACKUPS_DATABSE_NAME: z.string(),
 		BACKUPS_ORGANIZATION_SLUG: z.string(),
 		BACKUPS_CLOUDFLARE_ACCOUNT_ID: z.string({
 			description:
-				"Account ID for the Cloudflare account. Note that this must match what you are ",
+				"Account ID for the Cloudflare account. Note that this ID should be the same one the bucket is hosted in.",
 		}),
 		BACKUPS_BUCKET_ACCESS_KEY_ID: z.string(),
 		BACKUPS_BUCKET_SECRET_ACCESS_KEY: z.string(),
 		BACKUPS_DB_BEARER: z.string({
 			description:
-				"This is a bearer token to access the databases. For good practice, try to keep this one read only just in case.",
+				"This is a bearer token to access the databases. If serverless DB provider allows, try to make this token read-only.",
 		}),
 		BACKUPS_BUCKET_NAME: z.string(),
 	},
