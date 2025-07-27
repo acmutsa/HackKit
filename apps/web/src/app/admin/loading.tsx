35e762512267f4443e9f51467018ae7065072ceb
joshuasilva414 <joshuasilva414@gmail.com>
2025-07-27 17:16:01 -0500

    Replace admin loading fallback

---
+++ apps/web/src/app/admin/loading.tsx
@@ -0,0 +1,9 @@
+import Loading from "@/components/shared/Loading";
+
+export default function AdminLoading() {
+	return (
+		<div className="flex h-screen w-screen items-center justify-center">
+			<Loading />
+		</div>
+	);
+}
