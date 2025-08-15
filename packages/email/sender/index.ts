a0a434e91b8b9cd2e2747637146f91aab01b54af
xxmatthewandersonxx <92887765+mjanderson1227@users.noreply.github.com>
2025-08-14 20:32:14 -0500

    Added notification when email related environment variables are not present

--- packages/email/sender/index.ts
+++ packages/email/sender/index.ts
@@ -1,5 +1,11 @@
 import Plunk from "@plunk/node";
 
+if (!process.env.PLUNK_API_URL || !process.env.PLUNK_API_KEY) {
+	console.warn(
+		"Plunk API information is not defined... Did you add the relevant environment variables to the project?",
+	);
+}
+
 export const plunk = new Plunk(process.env.PLUNK_API_KEY as string, {
 	baseUrl: process.env.PLUNK_API_URL as string,
 });
