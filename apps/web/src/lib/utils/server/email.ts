a5bb7e691e3e7c59eadf16ec1b76c6441808dff5
joshuasilva414 <joshuasilva414@gmail.com>
2026-03-11 18:15:36 -0500

    Send registration confirmation email

--- apps/web/src/lib/utils/server/email.ts
+++ apps/web/src/lib/utils/server/email.ts
@@ -1,6 +1,7 @@
 import c from "config";
 import { smtpSender, etherealSender } from "email";
 import RSVPConfirmationEmail from "email/templates/rsvp-confirmation";
+import RegistrationSuccessEmail from "email/templates/registration-confirmation";
 
 // const mailer = smtpSender({
 // 	host: process.env.SMTP_HOST,
@@ -23,6 +24,21 @@ export const sendRSVPConfirmationEmail = async (email: string, props?: any) => {
 			subject: "RSVP Confirmation",
 			text: "You have been successfully RSVPed to the event!",
 			body: RSVPConfirmationEmail(props),
+		});
+	}
+};
+
+export const sendRegistrationSuccessEmail = async (
+	email: string,
+	props?: any,
+) => {
+	if (c.featureFlags.extra.emailService) {
+		await mailer.send({
+			from: "<[EMAIL_ADDRESS]>",
+			to: email,
+			subject: "Registration Confirmation",
+			text: "You have been successfully registered!",
+			body: RegistrationSuccessEmail(props),
 		});
 	}
 };
