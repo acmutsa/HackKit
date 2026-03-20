9639ad4fa6e8785f33ee03087ae464006c5e1efd
ExistentialKevin <shapes117@gmail.com>
2026-03-19 19:08:59 -0500

    Formatted w/ Prettier

--- apps/web/src/components/schedule/ScheduleTimelineBare.tsx
+++ apps/web/src/components/schedule/ScheduleTimelineBare.tsx
@@ -4,71 +4,71 @@ import c from "config";
 import { ReactNode } from "react";
 
 const daysOfWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
 ];
 
 function splitByDay(schedule: Event[]) {
-  const days: Map<string, Event[]> = new Map();
-  schedule.forEach((event) => {
-    const day = daysOfWeek[event.startTime.getDay()];
-    const existing = days.get(day);
-    if (existing) existing.push(event);
-    else days.set(day, [event]);
-  });
-  return days;
+	const days: Map<string, Event[]> = new Map();
+	schedule.forEach((event) => {
+		const day = daysOfWeek[event.startTime.getDay()];
+		const existing = days.get(day);
+		if (existing) existing.push(event);
+		else days.set(day, [event]);
+	});
+	return days;
 }
 
 type DayHeaderProps = { dayName: string };
 const DefaultDayHeader = ({ dayName }: DayHeaderProps) => (
-  <tr key={dayName + " title"} className="py-8 border-b">
-    <td>
-      <h2 className="px-4 w-full py-4 text-5xl font-black">
-        {dayName}
-      </h2>
-    </td>
-  </tr>
+	<tr key={dayName + " title"} className="border-b py-8">
+		<td>
+			<h2 className="w-full px-4 py-4 text-5xl font-black">{dayName}</h2>
+		</td>
+	</tr>
 );
 
 type RowProps = { event: Event; userTimeZone: string };
 
 export default function ScheduleTimelineBare({
-  schedule,
-  timezone,
-  DayHeader = DefaultDayHeader,
-  RenderEventRow,
+	schedule,
+	timezone,
+	DayHeader = DefaultDayHeader,
+	RenderEventRow,
 }: {
-  schedule: Event[];
-  timezone?: string;
-  DayHeader?: React.ComponentType<DayHeaderProps>;
-  RenderEventRow: React.ComponentType<RowProps>;
+	schedule: Event[];
+	timezone?: string;
+	DayHeader?: React.ComponentType<DayHeaderProps>;
+	RenderEventRow: React.ComponentType<RowProps>;
 }) {
-  const userTimeZone = timezone ?? getClientTimeZone(c.hackathonTimezone);
-  const days = Array.from(splitByDay(schedule).entries());
+	const userTimeZone = timezone ?? getClientTimeZone(c.hackathonTimezone);
+	const days = Array.from(splitByDay(schedule).entries());
 
-  return (
-    <div className="mx-auto mt-5 w-[99vw] lg:w-3/4">
-      <table className="w-full">
-        <tbody>
-          {days.map(([dayName, arr]): ReactNode => (
-            <>
-              <DayHeader dayName={dayName} />
-              {arr.map((event) => (
-                <RenderEventRow
-                  key={event.id}
-                  event={event}
-                  userTimeZone={userTimeZone}
-                />
-              ))}
-            </>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+	return (
+		<div className="mx-auto mt-5 w-[99vw] lg:w-3/4">
+			<table className="w-full">
+				<tbody>
+					{days.map(
+						([dayName, arr]): ReactNode => (
+							<>
+								<DayHeader dayName={dayName} />
+								{arr.map((event) => (
+									<RenderEventRow
+										key={event.id}
+										event={event}
+										userTimeZone={userTimeZone}
+									/>
+								))}
+							</>
+						),
+					)}
+				</tbody>
+			</table>
+		</div>
+	);
 }
