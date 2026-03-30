c5f90d2f438704b15219ebe79028f3c9ac839551
MKorolyova <mkorolyova52@gmail.com>
2026-03-30 18:44:21 -0500

    partner section, partners must be updated later

--- apps/web/src/components/landing/PartnerCard.tsx
+++ apps/web/src/components/landing/PartnerCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { Manuale } from "next/font/google";
 
 type Partner = {
 	name: string;
@@ -9,78 +10,57 @@ type Partner = {
 	tier: string;
 };
 
-type colorMap = {
-	key: string;
-	value: string;
-};
-
-// const tierBorderMap = {
-//   [Tier.Title]:           "w-[15rem]      sm:w-72           md:w-72       lg:w-80       2xl:w-[19rem]",
-//   [Tier.Gold]:            "w-[12.75rem]   sm:w-[14.75rem]   md:w-[16rem]  lg:w-72       2xl:w-[19rem]",
-//   [Tier.Silver]:          "w-[11rem]      sm:w-52           md:w-60       lg:w-[16rem]  2xl:w-[17rem] ",
-//   [Tier.Bronze]:          "w-32           sm:w-40           md:w-[12rem]  lg:w-[14rem]  2xl:w-[16rem]",
-//   [Tier.Rowdy_Partner]:   "w-[7rem]       sm:w-32           md:w-40       lg:w-[11rem]  2xl:w-[13rem]",
-//   [Tier.In_Kind_Partner]: "w-[6rem]       sm:w-[7rem]       md:w-32       lg:w-40       2xl:w-52",
-// };
-
-const tierColorMap: { [key: string]: string } = {
-	["Title Sponsor"]: "text-purple-500",
-	["Gold Sponsor"]: "text-yellow-600",
-	["Silver Sponsor"]: "text-gray-400",
-	["Bronze Sponsor"]: "text-amber-800",
-	["Rowdy Partner"]: "text-blue-500",
-	["Rowdy In-Kind"]: "text-red-500",
-};
+const manuale = Manuale({
+  weight: ["300", "400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+  variable: "--font-manuale",
+});
 
 function PartnerCard({
 	partner,
 	is_title,
+	index,
 }: {
 	partner: Partner;
 	is_title: boolean;
+	index: number;
 }) {
-	const text: string = is_title
-		? "text-2xl sm:text-3xl xl:text-4xl 2xl:text-[3rem]"
-		: "text-md sm:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl";
 
-	const height: string = is_title
-		? "h-[15rem] sm:h-[15rem] md:h-[16rem] lg:h-[20rem] xl:h-[20rem] 2xl:h-[22rem]"
-		: "h-[9rem] sm:h-[11rem] md:h-[11rem] lg:h-[12rem] xl:h-[14rem] 2xl:h-[17rem]";
-	const image: string = is_title
-		? "w-[17rem] sm:w-[17rem] md:w-[18rem] xl:w-[20rem] 2xl:w-[24rem]"
-		: "w-[8rem] sm:w-[10rem] md:w-[14rem] lg:w-48 xl:w-[16rem]";
-
+	const padding: string [] = [
+		"pl-[15vh] pt-[0vh]  pb-[2vh]                              lg:pt-[0vh]                            xl:pt-[5vh] xl:pb-[0vh]                    2xl:pl-[3vh] 2xl:pt-[7vh] 2xl:pb-[0vh]", 
+		"pl-[1vh]  pr-[30vw] pt-[0vh] pb-[2vh]                     lg:pt-[0vh] lg:pr-[2vw]                xl:pt-[2vh] xl:pr-[2vw]                   2xl:pl-[0vw] 2xl:pr-[0vw] ", 
+		"pl-[1vh]  pr-[30vw] pt-[0vh] pb-[0vh]    md:pr-[2vw]      lg:pr-[30vw]                           xl:pr-[40vw] xl:pt-[0vh] xl:pb-[2vh]      2xl:pr-[0vw] 2xl:pb-[7vh]", 
+		"pl-[1vh]  pt-[5vh]  pb-[0vh]             md:pt-[15vh]     lg:pt-[5vh] lg:pb-[0vh] lg:pb-[4vh]    xl:pr-[3vw] xl:pt-[0vh] xl:pb-[6vh]        2xl:pl-[0vw] 2xl:pr-[0vw]", 
+		"pl-[1vh]  pt-[0vh]  pb-[6vh]             md:pr-[4vw]      lg:pt-[0vh] lg:pb-[5vh] lg:pr-[4vw]    xl:pr-[4vw] xl:pt-[6vh] xl:pb-[0vh]       2xl:pr-[33vw]",
+		"pl-[1vh]  pt-[0vh]  pb-[4vh]                                                                     xl:pt-[5vh]                               2xl:pb-[0vh] 2xl:pt-[7vh]", 
+		"pl-[1vh]  pb-[2vh]", 
+	]
 	return (
 		<Link
 			href={partner?.url}
 			target="_blank"
-			className={`duration-350 group font-semibold opacity-100 transition ease-in-out hover:-translate-y-8 ${text} ${
-				tierColorMap[partner?.tier]
-			}`}
+			className={`duration-350 group h-fit ${padding[index] ?? padding[padding.length - 1]} font-semibold opacity-100 transition ease-in-out hover:scale-105`}
+			
 		>
 			<div
-				className={`flex w-full items-center justify-center ${height} rounded-lg bg-[#d2b48c] bg-opacity-45 p-3 shadow-none`}
+				className={`flex flex-col items-center justify-center w-[38vw] md:w-[35vw] lg:w-[32vw] xl:w-[23vw] 2xl:w-[23vw] h-[10vh] sm:h-[13vh] md:h-[15vh] lg:h-[20vh] 
+					bg-contain bg-center bg-no-repeat ${manuale.className} font-normal text-sm md:text-base lg:text-lg drop-shadow-[4px_6px_3px_rgba(0,0,0,0.45)]`}
+					 style={{backgroundImage: "url('/img/sponsors/empty-paper.svg')",}}
 			>
-				<Image
-					src={`/img/partner-logos/${partner?.logo}`}
-					width={0}
-					height={0}
-					quality={100}
-					priority={true}
-					alt={`${partner?.name} logo`}
-					className={`h-auto ${image}`}
-				/>
+				<div className="relative w-[70%] 2xl:w-[55%] h-[60%] shrink-0 mt-5">
+						<Image
+						src={`/img/partner-logos/${partner?.logo}`}
+						alt={`${partner?.name} logo`}
+						fill
+						className="object-contain"
+						/>
+				</div>
+
+				<p className="mb-5 flex items-center justify-center text-center">
+					{partner?.name}
+				</p>
 			</div>
-			<h2
-				className={`w-full text-center transition delay-100 duration-300 ease-in-out ${
-					is_title ? "pb-8" : "pb-4"
-				} invisible group-hover:visible group-hover:translate-y-4`}
-			>
-				{partner?.name}
-			</h2>
-			<h2 className="invisible w-full pb-4 text-center transition delay-75 duration-300 ease-in-out group-hover:visible group-hover:translate-y-4">
-				{partner?.tier}
-			</h2>
+
 		</Link>
 	);
 }
