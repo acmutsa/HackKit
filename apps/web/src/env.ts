5e146fdcf4c3ae9218359f71605361e5c6804029
Rufat00 <rufat.niftaliyev2007@gmail.com>
2026-06-25 11:18:13 -0500

    fixing env

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -22,6 +22,7 @@ export const env = createEnv({
 	client: {
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
 	},
+	skipValidation: process.env.NODE_ENV === "production",
 	experimental__runtimeEnv: {
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
 			process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
